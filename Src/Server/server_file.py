95c1c6212a408bc02921b72cc25bcb6caeabca6c
Nunite <1318205957@qq.com>
2024-10-22 02:27:45 +0800

    Server

---
+++ Src/Server/server_file.py
@@ -0,0 +1,27 @@
+import socket, os, setting
+
+
+def OpenServer():
+    # 创建socket:
+    s = socket.socket()
+    s.bind((setting.Serverhost, setting.Serverport))
+    s.listen(5)
+    print(f"客户端监听:{setting.Serverhost}:{setting.Serverport}")
+    # 获取信息:
+    client_socket, addr = s.accept()
+    print(f"接受客户端{addr}的连接")
+    FileInfo = client_socket.recv(setting.Chunk_Size).decode()
+    FileName, FileSize = FileInfo.split(setting.SEPARATOR)
+    FileSize = int(FileSize)
+    # 接受并创建文件:
+    with open(FileName, "+wb") as f:
+        while True:
+            FileContent = client_socket.recv(setting.Chunk_Size)
+            if not FileContent:
+                break
+            f.write(FileContent)
+    client_socket.close()
+    s.close()
+
+
+OpenServer()
--- Src/client/client_file.py
+++ Src/client/client_file.py
@@ -1,16 +1,28 @@
-import socket,os,tqdm
+import socket, os, setting
 
 
 def SendFile():
+    # 连接服务器:
+    s = socket.socket()
+    print(f"服务器连接中{setting.Serverhost}:{setting.Serverport}")
+    s.connect((setting.Serverhost, setting.Serverport))
     # 获取文件:
     print("请输入要传输的文件地址:")
-    filename = input()
-    filename_base = os.path.basename(filename)
-    print(f"filename={filename}")
-    print(f"filename_bsae={filename_base}")
-"""     # 建立连接:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('192.168.43.130', 7230)) """
-    
+    FileName = input()
+    FileName_Base = os.path.basename(FileName)
+    File_Size = os.path.getsize(FileName)
+    # 构建前缀:
+    datastr = f"{FileName_Base}{setting.SEPARATOR}{File_Size}"
+    s.send(datastr.encode())
+
+    with open(FileName, "rb") as f:
+        while True:
+            chunk = f.read(setting.Chunk_Size)
+            if not chunk:
+                break
+            s.sendall(chunk)
+    s.close()
+
+
 SendFile()
-#connect()
+# connect()
